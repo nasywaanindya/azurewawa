b9c82df768143180557dd188be8d40e78e4fe610
nasywaanindya <nasywanindyaes@gmail.com>
2025-03-20 02:01:40 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Import express
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
 
 // Middleware untuk parsing data form
 app.use(express.urlencoded({ extended: true }));
